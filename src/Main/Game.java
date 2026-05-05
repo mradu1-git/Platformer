39b87b8bdf62cdb0fdaf50bdadd8f45c4d0ba7e2
Mradul Pal <mradulpal@outlook.com>
2026-05-05 18:41:53 +0530

    Created Game structure

---
+++ src/Main/Game.java
@@ -0,0 +1,14 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class Game {
+    public static void main() {
+        JFrame window = new JFrame("Dragon Tale");
+        window.setContentPane(new GamePanel());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.pack();
+        window.setVisible(true);
+    }
+}
